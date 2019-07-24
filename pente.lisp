cf85556aaa97001a721f8a356f134131bd43ad50
Austin Smith <austin.smith1998@hotmail.com>
2019-07-23 23:58:21 -0400

    Initial commit.

--- pente.lisp
+++ pente.lisp
@@ -1,16 +1,42 @@
 (defparameter *max-captures* 10)
+(defparameter *white* 'W)
+(defparameter *black* 'B)
+(defparameter *empty* 'E)
+(defparameter *board-size* '(13 13))
 
-(defun make-board ()
-  ; TODO
-  )
+(defun move-x (move)
+  (car (car move)))
 
-(defun place-piece (x y color)
-  ; TODO
-  )
+(defun move-y (move)
+  (car (cdr (car move))))
 
-(defun five-in-row (board color)
-  ; TODO
-  )
+(defun move-coord (move)
+  (car move))
+
+(defun move-color (move)
+  (cdr move))
+
+(defun make-board-from-move-list (move-list)
+  (let ((board (make-array *board-size* :initial-element *empty*)))
+    (dolist (move move-list)
+      (setf (aref board (move-x move) (move-y move)) (move-color move)))
+    board))
+
+(defun remove-piece (x y move-list)
+  (remove-if (lambda (move)
+               (equal (move-coord move) (list x y)))
+             move-list))
+
+(defun place-piece (x y color move-list)
+  (cons (cons (list x y) color) move-list))
+
+(defun piece-at-space (x y move-list)
+  (find-if (lambda (move)
+             (equal (move-coord move) (list x y)))
+           move-list))
+
+(defun is-five-in-row (color move-list)
+  nil)
 
 (defun captured-enough-for-win (num-captured)
   (if (>= num-captured *max-captures*)
